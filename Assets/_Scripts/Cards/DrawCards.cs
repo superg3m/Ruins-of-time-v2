b708ec18d82b003ba85b658e57f7efbed1f0c955
superg3m <superg3m@gmail.com>
2022-09-02 21:56:45 -0400

    added deck

---
+++ Assets/_Scripts/Cards/DrawCards.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCards : MonoBehaviour
+{
+    public bool isClicked;
+    public void changeBool()
+    {
+        isClicked = true;
+    }
+}
--- Assets/_Scripts/Cards/SpawnCard.cs
+++ Assets/_Scripts/Cards/SpawnCard.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.TextCore.Text;
 using UnityEngine;
 
-public class DrawCard : MonoBehaviour
+public class SpawnCard : MonoBehaviour
 {
     public GameObject card;
+    public DrawCards drawCards;
     public Canvas canvas;
     private GameObject newCard;
     [SerializeField] private Transform spawnPoint;
 
     public void Start()
     {
-        newCard = Instantiate(card, spawnPoint.position, spawnPoint.rotation, canvas.transform);
+        
+        
     }
-    public void drawCards()
+    public void buttonPress()
     {
         
-       
+        Destroy(newCard);
+        newCard = Instantiate(card, spawnPoint.position, spawnPoint.rotation, canvas.transform);
     }
 }
