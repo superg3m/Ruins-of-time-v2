bced6ced618acfbbceaaabee1357352b505abea3
superg3m <superg3m@gmail.com>
2022-09-10 17:30:33 -0400

    Added actual hand

---
+++ Assets/_Scripts/Cards/GetHandList.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetHandList : MonoBehaviour
+{
+    public GameObject one;
+    public  GameObject two;
+    public  GameObject three;
+    public GameObject four;
+    public GameObject five;
+
+
+
+}
